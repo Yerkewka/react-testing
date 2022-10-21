7e9dfad724f8723db7a7d784647cb460872ab858
yerkebulan <y.serikbayev@ssa.group>
2022-10-21 11:51:17 +0600

    Add CounterTwo component and tests

---
+++ src/components/counter-two/CounterTwo.tsx
@@ -0,0 +1,20 @@
+interface IProps {
+  count: number;
+  handleIncrement?: () => void;
+  handleDecrement?: () => void;
+}
+
+export const CounterTwo: React.FC<IProps> = ({
+  count,
+  handleIncrement,
+  handleDecrement,
+}) => {
+  return (
+    <div>
+      <h1>Counter Two</h1>
+      <p>{count}</p>
+      {handleIncrement && <button onClick={handleIncrement}>Increment</button>}
+      {handleDecrement && <button onClick={handleDecrement}>Decrement</button>}
+    </div>
+  );
+};
